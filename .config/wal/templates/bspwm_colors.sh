95de79f76a2e7e84b060960cf047305a8a57f524
Loic Coyle <loic.coyle@hotmail.fr>
2020-08-29 01:51:17 +0200

    add wal

---
+++ .config/wal/templates/bspwm_colors.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+bspc config focused_border_color "{foreground}"
+bspc config normal_border_color  "{background}"
+bspc config active_border_color "{color8}"
+bspc config presel_feedback_color "{color8}"
