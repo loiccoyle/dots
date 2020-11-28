68c0acf5bdb5013fd689c70b2faf70734e5af1e5
Loic Coyle <loic.coyle@hotmail.fr>
2020-11-28 01:48:20 +0100

    colors

--- .config/bspwm/bspwm_colors.sh
+++ .config/bspwm/bspwm_colors.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-bspc config focused_border_color "#cad4da"
-bspc config normal_border_color  "#173434"
-bspc config active_border_color "#c1ccd3"
-bspc config presel_feedback_color "#c1ccd3"
+bspc config focused_border_color "#f5f4f3"
+bspc config normal_border_color  "#172134"
+bspc config active_border_color "#f4f3f2"
+bspc config presel_feedback_color "#f4f3f2"
