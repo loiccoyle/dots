c05b22003798d1454d81dfecc361c067a1e4a7a2
Loic Coyle <loic.coyle@hotmail.fr>
2021-01-16 23:59:46 +0100

    rm wal

--- .config/wal/templates/bspwm_colors.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-bspc config focused_border_color "{foreground}"
-bspc config normal_border_color  "{background}"
-bspc config active_border_color "{color8}"
-bspc config presel_feedback_color "{color8}"
