d153b4c9fca698cee0fe8e5f9f27d06cd095a22b
Loic Coyle <loic.coyle@hotmail.fr>
2021-03-24 22:39:03 +0100

    cleanup

--- .config/lf/scope.sh
+++ .config/lf/scope.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+#
+# lf preview script.
+# Adapted from: https://github.com/gokcehan/lf/wiki/Ranger
 
 set -C -f -u
 IFS=$'\n'
@@ -6,94 +9,93 @@ IFS=$'\n'
 # ANSI color codes are supported.
 # STDIN is disabled, so interactive scripts won't work properly
 
-# This script is considered a configuration file and must be updated manually.
-
-# Meanings of exit codes:
-# code | meaning    | action of ranger
-# -----+------------+-------------------------------------------
-# 0    | success    | Display stdout as preview
-# 1    | no preview | Display no preview at all
-# 2    | plain text | Display the plain content of the file
-
 # Script arguments
-FILE_PATH="${1}"         # Full path of the highlighted file
+FILE_PATH="${1}"  # Full path of the highlighted file
 
 FILE_EXTENSION="${FILE_PATH##*.}"
-FILE_EXTENSION_LOWER=$(echo ${FILE_EXTENSION} | tr '[:upper:]' '[:lower:]')
+FILE_EXTENSION_LOWER="$(printf "${FILE_EXTENSION}" | tr "[:upper:]" "[:lower:]")"
 
 # Settings
-HIGHLIGHT_SIZE_MAX=262143  # 256KiB
-# HIGHLIGHT_TABWIDTH=4
-# HIGHLIGHT_STYLE='/usr/share/highlight/themes/base16/solarized-dark.theme'
+# HIGHLIGHT_SIZE_MAX=262143  # 256KiB
+BSDTAR_SIZE_MAX=524288000  # 500 MiB
 
 
 handle_extension() {
+    # Preview based on the file extension.
     case "${FILE_EXTENSION_LOWER}" in
         # Archive
         a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|\
         rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z|zip)
-            atool --list -- "${FILE_PATH}"
+            if [ "$(stat --dereference --printf="%s" -- "${FILE_PATH}")" -gt "${BSDTAR_SIZE_MAX}" ]; then
+                # skip if bigger than 500 MiB
+                return 1
+            fi
             bsdtar --list --file "${FILE_PATH}"
-            exit 1;;
+            ;;
         rar)
             # Avoid password prompt by providing empty password
             unrar lt -p- -- "${FILE_PATH}"
-            exit 1;;
+            ;;
         7z)
             # Avoid password prompt by providing empty password
             7z l -p -- "${FILE_PATH}"
-            exit 1;;
+            ;;
 
-        # PDF
         pdf)
             # Preview as text conversion
             pdftotext -l 10 -nopgbrk -q -- "${FILE_PATH}" -
-            mutool draw -F txt -i -- "${FILE_PATH}" 1-10
-            exiftool "${FILE_PATH}"
-            exit 1;;
+            ;;
 
-        # BitTorrent
+        h5)
+            printf "HDF keys:\n"
+            h5ls "${FILE_PATH}"
+            ;;
+
         torrent)
             transmission-show -- "${FILE_PATH}"
-            exit 1;;
+            ;;
 
         # OpenDocument
         odt|ods|odp|sxw)
             # Preview as text conversion
             odt2txt "${FILE_PATH}"
-            exit 1;;
+            ;;
 
         # HTML
         htm|html|xhtml)
             # Preview as text conversion
-            w3m -dump "${FILE_PATH}"
-            lynx -dump -- "${FILE_PATH}"
+            w3m -dump "${FILE_PATH}" ||\
+            lynx -dump -- "${FILE_PATH}" ||\
             elinks -dump "${FILE_PATH}"
-            ;; # Continue with next handler on failure
+            ;;
+        *)
+            # Fall through and try the next handler
+            return 1
+            ;;
     esac
 }
 
 handle_mime() {
+    # Preview based on the mime type.
     local mimetype="${1}"
     case "${mimetype}" in
         # Text
         text/* | */xml | */json | */csv)
             # Syntax highlight
-            if [ "$( stat --printf='%s' -- "${FILE_PATH}" )" -gt "${HIGHLIGHT_SIZE_MAX}" ]; then
-                # Skip if file too long
-                cat "${FILE_PATH}"
-                exit 2
-            fi
+            # if [ "$(stat --dereference --printf="%s" -- "${FILE_PATH}" )" -gt "${HIGHLIGHT_SIZE_MAX}"]; then
+            #     # Skip if file too long
+            #     cat "${FILE_PATH}"
+            #     return
+            # fi
             # if [ "$( tput colors )" -ge 256 ]; then
-            #     local highlight_format='xterm256'
+            #     local highlight_format="xterm256"
             # else
-            #     local highlight_format='ansi'
+            #     local highlight_format="ansi"
             # fi
             # highlight --replace-tabs="${HIGHLIGHT_TABWIDTH}" --out-format="${highlight_format}" \
             #     --config-file="${HIGHLIGHT_STYLE}" --force -- "${FILE_PATH}"
-            highlight --style "$XDG_CONFIG_HOME/highlight/themes/flavours.theme" --out-format "ansi" --force -- "${FILE_PATH}"
-            # cat "${FILE_PATH}"
-            exit 2
+            highlight --style "$XDG_CONFIG_HOME/highlight/themes/flavours.theme" --out-format "ansi" --force -- "${FILE_PATH}" ||\
+            cat "${FILE_PATH}"
             ;;
 
         # Image
@@ -101,27 +103,26 @@ handle_mime() {
             # Preview as text conversion
             # img2txt --gamma=0.6 -- "${FILE_PATH}" && exit 1
             exiftool "${FILE_PATH}"
-            exit 1
             ;;
 
         # Video and audio
         video/* | audio/* | application/octet-stream)
             # mediainfo "${FILE_PATH}"
             exiftool "${FILE_PATH}"
-            exit 1
+            ;;
+        *)
+            # Fall through and try the next handler
+            return 1
             ;;
     esac
 }
 
 handle_fallback() {
-    echo '----- File Type Classification -----' && file --dereference --brief -- "${FILE_PATH}"
-    exit 1
+    echo "----- File Type Classification -----"
+    file --dereference --brief -- "${FILE_PATH}"
 }
 
 
-MIMETYPE="$( file --dereference --brief --mime-type -- "${FILE_PATH}" )"
-handle_extension
-handle_mime "${MIMETYPE}"
-handle_fallback
-
-exit 1
+MIMETYPE="$(file --dereference --brief --mime-type -- "${FILE_PATH}")"
+# Try handlers and stop when one doesn't fail
+handle_extension || handle_mime "${MIMETYPE}" || handle_fallback
