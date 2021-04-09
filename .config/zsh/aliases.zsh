5e36c69cab457b8c91385c73200eb0f77cb56691
Loic Coyle <loic.coyle@hotmail.fr>
2021-04-09 18:34:16 +0200

    remove config edit aliases, add git-edit executable
    use `dots edit` to edit dotfiles

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -9,44 +9,41 @@
 # | (_| | | | (_| \__ \  __/\__ \
 #  \__,_|_|_|\__,_|___/\___||___/
 
-# Aliases
+# Quality of life
 alias ls="ls --color=auto --group-directories-first"
 alias ll="ls -lha"
 alias l="ls -lh"
 alias cp="cp -i"  # Confirm before overwriting something
 alias mv="mv -i"  # Confirm before overwriting something
 alias rm="rm -i"  # Confirm on removal
-alias df='df -h'  # Human-readable sizes
-alias free='free -h'  # Human-readable sizes
+alias df="df -h"  # Human-readable sizes
+alias free="free -h"  # Human-readable sizes
 alias open="$OPENER"
 # pacman
-alias psyu='sudo pacman -Syu'
-alias pacdiff='sudo -E pacdiff'
+alias psyu="sudo pacman -Syu"
+alias pacdiff="sudo -E pacdiff"
 # full upgrade
-alias sysupdate='yay && pipx upgrade-all && zinit update --all && vim --headless +PlugUpdate +qall > /dev/null'
+alias sysupdate="yay &&\
+    pipx upgrade-all &&\
+    zinit self-update && zinit update --all &&\
+    vim --headless +PlugUpgrade +PlugUpdate +qall > /dev/null"
 # python venv
 alias venv="python3 -m venv"
 alias de="deactivate"
 alias ipy="ipython"
 # python jupyter notebook
 alias jup="jupyter notebook"
-# config edits
-alias czsh="$EDITOR $ZDOTDIR"
-alias clf="$EDITOR $XDG_CONFIG_HOME/lf/lfrc"
-alias cwm="$EDITOR $XDG_CONFIG_HOME/bspwm/bspwmrc"
-alias chk="$EDITOR $XDG_CONFIG_HOME/sxhkd/sxhkdrc"
-alias cvim="$EDITOR $XDG_CONFIG_HOME/nvim/init.vim"
 # dotfile git
-alias dots='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-alias lgdots='lazygit --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias dots="git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+alias lgdots="lazygit --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 # vimwiki
-alias vimwiki="vim -c \"VimwikiIndex\""
+alias vimwiki="vim +VimwikiIndex"
 # fasd
-alias v='fasd -f -e vim'
+alias v="fasd -f -e vim"
 # colours
 alias diff="diff --color=auto"
 alias grep="grep --colour=auto"
-# git aliases
+# git
 alias ga="git add"
 alias gc="git commit"
 alias gp="git push"
@@ -54,11 +51,11 @@ alias gs="git status"
 alias lg="lazygit"
 
 # Functions
-edit () {
+edit() {
     # Open command in EDITOR
     $EDITOR "$(which "$1")"
 }
-ae () {
+ae() {
     # Activate python venv
     if [ -f "venv/bin/activate" ]
     then
