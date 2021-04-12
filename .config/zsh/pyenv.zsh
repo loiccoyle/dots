5867aa936d3471c88bf640b3c530b777ddbe5cdf
Loic Coyle <loic.coyle@hotmail.fr>
2021-04-12 19:34:17 +0200

    add pyenv

---
+++ .config/zsh/pyenv.zsh
@@ -0,0 +1,26 @@
+# Setup pyenv
+if command -v pyenv 1>/dev/null 2>&1; then
+    export PYENV_ROOT="$XDG_DATA_HOME/pyenv"
+    eval "$(pyenv init -)"
+fi
+
+# pyenv completions
+# https://raw.githubusercontent.com/pyenv/pyenv/master/completions/pyenv.zsh
+if [[ ! -o interactive ]]; then
+    return
+fi
+
+compctl -K _pyenv pyenv
+
+_pyenv() {
+  local words completions
+  read -cA words
+
+  if [ "${#words}" -eq 2 ]; then
+    completions="$(pyenv commands)"
+  else
+    completions="$(pyenv completions ${words[2,-2]})"
+  fi
+
+  reply=(${(ps:\n:)completions})
+}
