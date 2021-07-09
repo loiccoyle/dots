3fe902a7bc77a89c8244c38fd96da1e5bdf0c4d6
Loic Coyle <loic.coyle@hotmail.fr>
2021-07-09 16:30:33 +0200

    polybar: siji -> font-awesome

--- .config/polybar/scripts/updates-arch-combined.sh
+++ .config/polybar/scripts/updates-arch-combined.sh
@@ -1,21 +1,31 @@
 #!/bin/sh
 #
 # Check for pakages updates, from pacman and the AUR.
-# Requires pacman-contrib, paru
+# Requires pacman-contrib, paru, dunstify (optional).
 
-if ! UPDATES_ARCH=$(checkupdates 2> /dev/null | wc -l ); then
-    UPDATES_ARCH=0
-fi
+UPDATE_FILE_TMP="/tmp/polybar_updates_arch_combined"
+[ ! -f "$UPDATE_FILE_TMP" ] && printf "0\n" >"$UPDATE_FILE_TMP"
 
-# if ! UPDATES_AUR=$(yay -Qum 2> /dev/null | wc -l); then
-if ! UPDATES_AUR=$(paru -Qum 2> /dev/null | wc -l); then
-    UPDATES_AUR=0
-fi
+UPDATES_ARCH="$(checkupdates 2>/dev/null | wc -l)" || UPDATES_ARCH=0
+UPDATES_AUR="$(paru -Qum 2>/dev/null | wc -l)" || UPDATES_AUR=0
 
 UPDATES=$(("$UPDATES_ARCH" + "$UPDATES_AUR"))
 
+PREVIOUS_UPDATES="$(cat "$UPDATE_FILE_TMP")"
+if [ "$UPDATES" -gt "$PREVIOUS_UPDATES" ]; then
+    if type dunstify >/dev/null; then
+        [ "$(dunstify -i "system-software-update" \
+            "Software Update" \
+            "$UPDATES update(s) available." \
+            --action="update,udpate")" = "update" ] && $TERMINAL --hold --class=float -e "paru" &
+    else
+        notify-send -i "system-software-update" "Software Update" "$UPDATES update(s) available."
+    fi
+fi
+printf "%s\n" "$UPDATES" >"$UPDATE_FILE_TMP"
+
 if [ "$UPDATES" -gt 0 ]; then
-    printf " %s" "$UPDATES"
+    printf "%s\n" "$UPDATES"
 else
     printf "\n"
 fi
