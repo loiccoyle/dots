8ba0d2dfbc2ee38e06d1f97b3a6cb373946ee550
Loic Coyle <loic.coyle@hotmail.fr>
2021-08-23 20:02:00 +0200

    bspwm: tweak border colors

--- .config/bspwm/bspwm_colors.sh
+++ .config/bspwm/bspwm_colors.sh
@@ -2,5 +2,5 @@
 
 bspc config focused_border_color "#ebdbb2"
 bspc config normal_border_color  "#1d2021"
-bspc config active_border_color "#d5c4a1"
-bspc config presel_feedback_color "#d5c4a1"
+bspc config active_border_color "#83a598"
+bspc config presel_feedback_color "#83a598"
