d42e3ed322ca65a96f9bb58ffe9270222e5cfbba
Loic Coyle <loic.coyle@hotmail.fr>
2021-08-29 19:48:50 +0200

    bspwm: update colours to recent template

--- .config/bspwm/bspwm_colors.sh
+++ .config/bspwm/bspwm_colors.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 bspc config focused_border_color "#ebdbb2"
-bspc config normal_border_color  "#1d2021"
+bspc config normal_border_color "#1d2021"
 bspc config active_border_color "#83a598"
 bspc config presel_feedback_color "#83a598"
