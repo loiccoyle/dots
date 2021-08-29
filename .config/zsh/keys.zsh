9c0c65678d889878aa339e19b4fbb7d7e558973a
Loic Coyle <loic.coyle@hotmail.fr>
2021-08-29 15:23:19 +0200

    zsh: shfmt

--- .config/zsh/keys.zsh
+++ .config/zsh/keys.zsh
@@ -17,40 +17,41 @@ bindkey -v
 export KEYTIMEOUT=1
 
 # Use lf to switch directories and bind it to ctrl-o
-lfcd () {
+lfcd() {
     tmp="$(mktemp)"
     lf -last-dir-path="$tmp" "$@"
     if [ -f "$tmp" ]; then
         dir="$(cat "$tmp")"
         rm -f "$tmp"
-        [ -d "$dir" ] && [ "$dir" != "$(pwd)" ] && cd "$dir"
+        [ -d "$dir" ] && [ "$dir" != "$(pwd)" ] && cd "$dir" || return
     fi
 }
 bindkey -s '^o' 'lfcd\n'
 # Home and end
-bindkey '^[[F' end-of-line  # End key
-bindkey '^[[H' beginning-of-line  # Home key
+bindkey '^[[F' end-of-line       # End key
+bindkey '^[[H' beginning-of-line # Home key
 # delete key
 bindkey '^[[3~' delete-char
 # Navigate words with ctrl+arrow keys
-bindkey '^[Oc' forward-word  # forward word with ctrl-left
-bindkey '^[Od' backward-word  # back word with ctrl-left
+bindkey '^[Oc' forward-word     # forward word with ctrl-left
+bindkey '^[Od' backward-word    # back word with ctrl-left
 bindkey '^[[1;5C' forward-word  # forward word with ctrl-left
-bindkey '^[[1;5D' backward-word  # back word with ctrl-left
-bindkey '^H' backward-kill-word  # delete previous word with ctrl+backspace
-bindkey '^[[3;5~' kill-word  # delete word with ctrl-delete
+bindkey '^[[1;5D' backward-word # back word with ctrl-left
+bindkey '^H' backward-kill-word # delete previous word with ctrl+backspace
+bindkey '^[[3;5~' kill-word     # delete word with ctrl-delete
 # Use vim keys in tab complete menu:
 bindkey -M menuselect 'h' vi-backward-char
 bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
 bindkey -M menuselect 'j' vi-down-line-or-history
 bindkey -v '^?' backward-delete-char
-bindkey '^[[Z' reverse-menu-complete  # Shift+tab
+bindkey '^[[Z' reverse-menu-complete # Shift+tab
 # fzf keys: ctrl + t, alt + c, ctrl + r
 [ -f "/usr/share/fzf/key-bindings.zsh" ] && . /usr/share/fzf/key-bindings.zsh
 # edit line in $EDITOR
-autoload edit-command-line; zle -N edit-command-line
-bindkey '^e' edit-command-line  # ctrl-e to edit line in editor
+autoload edit-command-line
+zle -N edit-command-line
+bindkey '^e' edit-command-line # ctrl-e to edit line in editor
 # history substring search
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
--- .config/zsh/pyenv.zsh
+++ .config/zsh/pyenv.zsh
@@ -15,14 +15,14 @@ fi
 compctl -K _pyenv pyenv
 
 _pyenv() {
-  local words completions
-  read -cA words
+    local words completions
+    read -cA words
 
-  if [ "${#words}" -eq 2 ]; then
-    completions="$(pyenv commands)"
-  else
-    completions="$(pyenv completions ${words[2,-2]})"
-  fi
+    if [ "${#words}" -eq 2 ]; then
+        completions="$(pyenv commands)"
+    else
+        completions="$(pyenv completions ${words[2,-2]})"
+    fi
 
-  reply=(${(ps:\n:)completions})
+    reply=(${(ps:\n:)completions})
 }
