0f6cbf5e8263294094a3f0ad3accf95369f596d3
Loic Coyle <loic.coyle@hotmail.fr>
2021-12-03 01:32:23 +0100

    nvim: nvim 0.6 update, diagnostic rename

--- .config/nvim/lua/plugins/nvim-lspconfig/init.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig/init.lua
@@ -37,9 +37,9 @@ lsp_installer.on_server_ready(function(server)
     vim.cmd([[ do User LspAttachBuffers ]])
 end)
 
-local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
-    local hl = "LspDiagnosticsSign" .. type
+    local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
