1acf057f1c2bbf6532294d742c2fc16e0bc9c6be
Loic Coyle <loic.coyle@hotmail.fr>
2022-08-09 18:06:50 +0200

    nvim(cmp): disable autopair in sh

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -96,4 +96,4 @@ cmp.setup.cmdline("/", {
 
 -- autopairs
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "", sh = "" } }))
