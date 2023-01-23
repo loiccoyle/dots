838e69a949f0625dd08c234502f0bd85806ddf6c
Loic Coyle <loic.coyle@hotmail.fr>
2023-01-23 21:36:58 +0100

    zsh: add docker aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -52,6 +52,8 @@ alias gc="git commit"
 alias gp="git push"
 alias gs="git status"
 alias lg="lazygit"
+type docker >/dev/null && alias docker="sudo docker"
+type lazydocker >/dev/null && alias lazydocker="sudo lazydocker"
 # other
 alias wifiqr="nmcli dev wifi show-password"
 
