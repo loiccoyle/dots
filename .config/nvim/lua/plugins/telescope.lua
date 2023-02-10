ec0d2e2891bcf889d93752462674bf49c8399041
Loic Coyle <loic.coyle@hotmail.fr>
2023-02-10 17:16:24 +0100

    fix(nvim): default telescope mappings now include the <c-j> <c-k> and q mappings

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -7,25 +7,6 @@ return {
     build = "make",
   },
   keys = keymaps.telescope,
-  opts = {
-    defaults = {
-      mappings = {
-        i = {
-          ["<C-j>"] = function(...)
-            require("telescope.actions").move_selection_next(...)
-          end,
-          ["<C-k>"] = function(...)
-            require("telescope.actions").move_selection_previous(...)
-          end,
-        },
-        n = {
-          ["q"] = function(...)
-            return require("telescope.actions").close(...)
-          end,
-        },
-      },
-    },
-  },
   config = function(_, opts)
     require("telescope").setup(opts)
     require("telescope").load_extension("notify")
