27339beb0a7e55209a2fd1c0359e7973e7818d25
Loic Coyle <loic.coyle@hotmail.fr>
2023-02-27 15:34:18 +0100

    zsh: update block cursor on vi-cmd function

--- .config/zsh/cursor.zsh
+++ .config/zsh/cursor.zsh
@@ -1,19 +1,21 @@
-# Change cursor shape for different vi modes.
-function zle-keymap-select {
-    if [[ ${KEYMAP} == vicmd ]] \
-        || [[ $1 == 'block' ]]; then
-        echo -ne '\e[1 q'
-    elif [[ ${KEYMAP} == main ]] \
-        || [[ ${KEYMAP} == viins ]] \
-        || [[ ${KEYMAP} == '' ]] \
-        || [[ $1 == 'beam' ]]; then
-        echo -ne '\e[5 q'
-    fi
+# ZLE hooks for prompt's vi mode status
+function zle-line-init zle-keymap-select {
+	# Change the cursor style depending on keymap mode.
+	case $KEYMAP {
+		vicmd)
+			printf '\e[0 q' # Box.
+			;;
+
+		viins|main)
+			printf '\e[6 q' # Vertical bar.
+			;;
+	}
 }
+zle -N zle-line-init
 zle -N zle-keymap-select
 
 _fix_cursor() {
-    echo -ne '\e[5 q'
+    echo -ne '\e[6 q'
 }
 # run it to force the correct cursor
 _fix_cursor
