ed169c84c266b40c9f6a040e8029298a29dc5aa9
Loic Coyle <loic.coyle@hotmail.fr>
2023-03-18 15:33:07 +0100

    fix(nvim): add mappings and lsp capabilities

--- .config/nvim/lua/plugins/ufo.lua
+++ .config/nvim/lua/plugins/ufo.lua
@@ -54,4 +54,13 @@ return {
       },
     },
   },
+  init = function()
+    -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+    vim.keymap.set("n", "zR", function()
+      require("ufo").openAllFolds()
+    end)
+    vim.keymap.set("n", "zM", function()
+      require("ufo").closeAllFolds()
+    end)
+  end,
 }
