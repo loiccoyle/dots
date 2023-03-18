3efb773f156cc60dfec57f0ca1d74bb64e13ef1b
Loic Coyle <loic.coyle@hotmail.fr>
2023-03-18 15:37:51 +0100

    fix(nvim): don't use matchup in shell scripts

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
     opts.rainbow = { enable = true, disable = { "html" } }
-    opts.matchup = { enable = true }
+    opts.matchup = { enable = true, disable = { "sh", "bash" } }
     ---@diagnostic disable-next-line: missing-parameter
     vim.list_extend(opts.ensure_installed, {
       "bibtex",
