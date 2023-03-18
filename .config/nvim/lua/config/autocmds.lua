7f8284ed55d47952deba32aeb8d0767f2b611977
Loic Coyle <loic.coyle@hotmail.fr>
2023-03-18 15:38:46 +0100

    fix(nvim): remove r formatoption

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,8 +1,9 @@
+---@diagnostic disable: param-type-mismatch
 local au_filetypes = vim.api.nvim_create_augroup("ConfigFileType", { clear = true })
 vim.api.nvim_create_autocmd(
   { "BufRead", "BufNewFile" },
   -- See https://github.com/LazyVim/LazyVim/issues/80
-  { group = au_filetypes, pattern = { "*" }, command = "set fo-=ro" }
+  { group = au_filetypes, pattern = { "*" }, command = "set fo-=o" }
 )
 vim.api.nvim_create_autocmd(
   { "BufRead", "BufNewFile" },
