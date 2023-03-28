00a908d4bbc2b3a4820f973f60b44edcf93aca08
Loic Coyle <loic.coyle@hotmail.fr>
2023-03-28 23:11:37 +0200

    fix(waybar): cleanup waybar gh script

--- .config/waybar/scripts/github.sh
+++ .config/waybar/scripts/github.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-token="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"
-count=$(curl -s -u loiccoyle:${token} https://api.github.com/notifications | jq '. | length')
+set -e
+
+token="$GH_NOTIF_TOKEN"
+count="$(curl -s -u "loiccoyle:${token} https://api.github.com/notifications" | jq '. | length')"
 tooltip='Open GitHub notifications'
 
 if [[ "$count" != "0" ]]; then
