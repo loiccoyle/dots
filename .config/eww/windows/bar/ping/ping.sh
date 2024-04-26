d4cbb67f9ddc55923da2144e357c1b3b905e03d8
Loic Coyle <loic.coyle@hotmail.fr>
2024-04-27 01:07:41 +0200

    fix(eww): zero pad ping

--- .config/eww/windows/bar/ping/ping.sh
+++ .config/eww/windows/bar/ping/ping.sh
@@ -7,5 +7,5 @@ if ! PING=$(ping -n -c 1 -W 1 "$HOST" 2>/dev/null); then
 else
     RTT=$(echo "$PING" | sed -rn 's/.*time=([0-9]{1,})\.?[0-9]{0,} ms.*/\1/p')
     [ -z "$RTT" ] && echo "" && exit
-    echo "$RTT ms"
+    printf "%02d ms" "$RTT"
 fi
