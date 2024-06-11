d338b0edb362bcfb4ffe21c4ab942a6ada420c66
Loic Coyle <loic.coyle@hotmail.fr>
2024-06-11 17:17:06 +0200

    fix(nvim): rm old stuff

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,8 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
-    opts.rainbow = { enable = true, disable = { "html" } }
-    opts.matchup = { enable = true, disable = { "sh", "bash" } }
     ---@diagnostic disable-next-line: missing-parameter
     vim.list_extend(opts.ensure_installed, {
       "bibtex",
@@ -22,7 +20,4 @@ return {
       "toml",
     })
   end,
-  dependencies = {
-    { "HiPhish/nvim-ts-rainbow2" },
-  },
 }
