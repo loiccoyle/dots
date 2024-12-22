dfa285ebb563bbc0f0162ebd9b3545325efedf37
Loic Coyle <loic.coyle@hotmail.fr>
2024-12-22 15:47:56 +0100

    fix(nvim): disable useless options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,11 +1,12 @@
 vim.opt.wrap = true
 -- Folds
-vim.opt.foldcolumn = "0" -- '0' is not bad
-vim.opt.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-vim.opt.foldlevelstart = 99
-vim.opt.foldenable = true
+-- vim.opt.foldcolumn = "0" -- '0' is not bad
+-- vim.opt.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+-- vim.opt.foldlevelstart = 99
+-- vim.opt.foldenable = true
 -- Tabs
 -- vim.opt.expandtab = false
 -- vim.opt.shiftwidth = 4
 -- vim.opt.softtabstop = 4
 -- vim.opt.tabstop = 4
+vim.g.snacks_animate = false
